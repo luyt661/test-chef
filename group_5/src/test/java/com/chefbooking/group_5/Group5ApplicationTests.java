9b15f3b987d2bb5ff3a37db50dddfe7873cee229
Nguyen Quoc Long <longnguyenquocdn@gmail.com>
2025-11-04 14:12:19 +0700

    Initial commit: Setup Spring Boot base project

---
+++ group_5/src/test/java/com/chefbooking/group_5/Group5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chefbooking.group_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
